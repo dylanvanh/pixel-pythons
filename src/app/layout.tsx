b6f945121282af00bde663eb0c3f3a0922279388
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2025-04-07 22:53:23 +0200

    fix footer in layout, delete unused ConnectWallet component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,27 +32,26 @@ export default function RootLayout({
         <LaserEyesProvider>
           {children}
           <AddressDebug />
+
+          <footer>
+            <div className="fixed bottom-0 left-0 right-0 bg-gray-100 border-t border-gray-300 p-1.5 text-center opacity-70 hover:opacity-100 transition-opacity">
+              <div className="flex justify-center items-center max-w-xl mx-auto">
+                <p className="text-[10px] text-gray-600">Use at your own risk.</p>
+                <div className="h-3 mx-3 border-r border-gray-300"></div>
+                <a
+                  href="https://github.com/dylanvanh/ordinal-mint"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[10px] text-gray-600 hover:text-gray-900 transition-colors"
+                  aria-label="GitHub"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          </footer>
         </LaserEyesProvider>
       </body>
-
-<footer>
-
-      <div className="fixed bottom-0 left-0 right-0 bg-gray-100 border-t border-gray-300 p-1.5 text-center opacity-70 hover:opacity-100 transition-opacity">
-        <div className="flex justify-center items-center max-w-xl mx-auto">
-          <p className="text-[10px] text-gray-600">Use at your own risk.</p>
-          <div className="h-3 mx-3 border-r border-gray-300"></div>
-          <a
-            href="https://github.com/dylanvanh/ordinal-mint"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-[10px] text-gray-600 hover:text-gray-900 transition-colors"
-            aria-label="GitHub"
-          >
-            GitHub
-          </a>
-        </div>
-      </div>
-</footer>
     </html>
   );
 }
