79c6765eb6526b27d5c280c898a69db6eae8f232
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2025-09-20 11:17:32 +0200

    feat: add docker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@napi-rs/canvas'],
+  output: 'standalone',
   /* config options here */
 };
 
